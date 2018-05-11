3108c23543fae8eecebf2f830d27f72e3169a02c
김성준 <virusboss@gmail.com>
2018-05-11 17:30:08 +0900

    다했습니다. 조장님

--- main.c
+++ main.c
@@ -37,23 +37,23 @@ int main(int argc, char* argv[])
 int add(int number1, int number2)
 {
 	//TODO implementation
-	return 0;
+	return number1 + number2;
 }
  
 int sub(int number1, int number2)
 {
 	//TODO implementation
-	return 0;
+	return number1-number2`;
 }
  
 int multi(int number1, int number2)
 {
 	//TODO implementation
-	return 0;
+	return number1*number2;
 }
  
 int div(int number1, int number2)
 {
 	//TODO implementation
-	return 0;
+	return number1/number2;
 }
